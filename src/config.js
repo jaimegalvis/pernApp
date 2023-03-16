230c8e514fd0b3d40f9f8f22250160540b9a888e
Jaime Galvis <jaime.galvis@techedgegroup.com>
2023-03-16 10:34:52 -0500

    Se agregan variables de entorno para DB

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config()
 
 module.exports = {
     db: {
-        user: process.env.DB_USER ||'postgres',
+        user: process.env.DB_USER || 'postgres',
         password: process.env.DB_PASSWORD || 'password',
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || '5432',
